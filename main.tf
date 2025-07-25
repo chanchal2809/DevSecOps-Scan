1286e46a330014be35baf76bfcafb1882ef4d1ea
chanchal <chanchal2809@icloud.com>
2025-07-25 13:55:47 +0100

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "statebucket28" {
-  bucket = "statebucket28"
+resource "aws_s3_bucket" "statebucket31" {
+  bucket = "statebucket31"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "statebucket31"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
